f714d968b3e348ac247a9204082712517f0a0a81
Лазарь Юрий Евгеньевич <27103@step.edu>
2017-07-10 20:50:52 +0300

    Add 100717 20-50

--- ChatSolution/ChatServise/ChatService.cs
+++ ChatSolution/ChatServise/ChatService.cs
@@ -11,52 +11,29 @@ namespace ChatServise
 {
     public class ChatService : IChatService
     {
-     //   List<IChatCallback> call = new List<IChatCallback>();
         Dictionary<string, IChatCallback> Call = new Dictionary<string, IChatCallback>();//для запоминания подключенных пользователей (онлайн юзеры)
-        public Thread t;//потоки для дуплекса
 
         
         public LoginObjectOut Login(LoginObjectIn obj)//войти
         {
-            //if (Call.Count!=0)
+
+            //if (BuisenessLevel.Login(obj) == LoginResult.Ok)
             //{
-            //    foreach (var item in Call)
-            //    {
-            //        ChatCallback ch = new ChatCallback();
-            //        ch.Callback = item.Value;//изъяли канал обр вызова IChatCallback из Dictionary
-            //        t = new Thread(new ParameterizedThreadStart(ch.UserJoined));
-            //        t.IsBackground = true;
-            //        t.Start(_Login);
-            //    }
 
-            //    //  void SetUsers(UsersobjectIn);//метод который запускается методом Login (при входе пользователя) пользователю который только залогинился придут пользователи которые онлайн
-            //    //послать новому пользователю список пользователей онлайн
-            //    ChatCallback newch = new ChatCallback();
-            //    newch.Callback = OperationContext.Current.GetCallbackChannel<IChatCallback>();                
-            //    foreach (var item in Call)
-            //    {
-            //        t=new Thread(new ParameterizedThreadStart(newch.SetUsers));
-            //        t.IsBackground = true;
-            //        t.Start(item.Key);
-            //    }
+                IChatCallback call = OperationContext.Current.GetCallbackChannel<IChatCallback>();
+                UsersObjectIn obj = new UsersObjectIn();
+                call.SetUsers(obj);
+                call.SetMessage(MessagesObjectIn obj);
+                Call.Add(obj, call);//добавили в базу онлайн пользователей
 
-            //    //метод который запускается методом Login (при входе пользователя) и показывает пользователю непрочитанные сообщения
-            //    t = new Thread(new ParameterizedThreadStart(newch.SetMessage));
-            //    t.IsBackground = true;
-            //    t.Start(_Login);
             //}
 
-            //Call.Add(_Login, OperationContext.Current.GetCallbackChannel<IChatCallback>());//добавили в базу онлайн пользователей
-            //Console.WriteLine(_Login +" вошел в чат");           
-
-            //return BuisenessLevel.Login(_Login);
             return new LoginObjectOut();
         }
 
         public void Logout(LogoutObjectIn obj)//выйти
         {
             //Call.Remove(_Logout);//исключили из базы онлайн пользователей
-            //Console.WriteLine(_Logout + " вышел из чата");
 
             //if (Call.Count != 0)
             //{
@@ -98,80 +75,7 @@ namespace ChatServise
         //public void StopStream()//для остановки потока
         //{
         //    t.Abort();
-        //    Console.WriteLine("Поток остановлен");
         //}
 
     }
-    //---------------------------------переместить в клиент--------------------
-        // //////////////////////////////////////////////ДУПЛЕКС//////////////////////////////////////////////////////////////////////
-    public class ChatCallback
-    {
-        public IChatCallback Callback = null;//ссылка на функцию - интерфейсная ссылка (потомучто нельзя создать экземпляр интерфейса) которая будет вызываться при калбеке
-
-        public void UserJoined(UserJoinedObjectIn obj)//разослать всем что юзер присоеденился к чату
-        {
-            try
-            {
-                UserJoinedObjectIn us = (UserJoinedObjectIn)obj;
-                //Callback.UserJoined(us);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Ошибка UserJoined" + ex.Message);
-            }
-        }
-
-        public void UserLeaved(UserLeavedObjectIn obj)//разослать всем что юзер покинул чат
-        {
-            try
-            {
-                UserLeavedObjectIn us = (UserLeavedObjectIn)obj;
-                //Callback.UserLeaved(us);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Ошибка UserLeaved" + ex.Message);
-            }
-        }
-
-        public void Message(MessageObjectIn obj)//разослать всем такое то сообщение (у нас общий чат)
-        {
-            try
-            {
-                MessageObjectIn us = (MessageObjectIn)obj;
-                //Callback.Message(us);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Ошибка Message" + ex.Message);
-            }
-        }
-
-       public void SetUsers(UsersObjectIn obj)//метод который запускается методом Login (при входе пользователя) пользователю который только залогинился придут пользователи которые онлайн
-        {
-            try
-            {
-                UsersObjectIn us = (UsersObjectIn)obj;
-                //Callback.Message(us);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Ошибка SetUsers" + ex.Message);
-            }
-        }
-
-        public void SetMessage(MessagesObjectIn obj)//метод который запускается методом Login (при входе пользователя) и показывает пользователю непрочитанные сообщения
-        {
-            try
-            {
-                MessagesObjectIn us = (MessagesObjectIn)obj;
-                //Callback.Message(us);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Ошибка SetMessage" + ex.Message);
-            }
-        }
-
-    }////////////////////////////////////////////////////////////Конец дуплекса/////////////////////////////////////////////////////////////////////
-}
+    }
