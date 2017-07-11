e5d55266c9a9a6270dffe8fcb4ccc88ac81620ca
Тылля Анастасия Игоревна <27112@step.edu>
2017-07-11 20:43:37 +0300

    no message

--- ChatSolution/ChatClient/ViewModel/ChatViewModel.cs
+++ ChatSolution/ChatClient/ViewModel/ChatViewModel.cs
@@ -96,7 +96,8 @@ namespace ChatClient.ViewModel
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            if(PropertyChanged!=null)
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
