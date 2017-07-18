672d144ddf59615a0864c4920f22ae35ad95ce6c
Home <home@gmail.com>
2017-07-18 14:35:10 +0300

    no message

---
+++ ChatSolution/DataLevel/AuthorizationDataManager.cs
@@ -0,0 +1,28 @@
+﻿using DataLevel.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLevel
+{
+    class AuthorizationDataManager : BaseDataManager
+    {
+        public LoginResultDataModel Login(LoginDataModel obj)
+        {
+            return new LoginResultDataModel { Result = (LoginResultData)_chatDb.Login(obj.Hash).FirstOrDefault() };
+        }
+        public void Logout(LogoutDataModel obj)
+        {
+            _chatDb.Logout(obj.UserId);
+        }
+        public RegistrationResultDataModel Registration(RegistrationDataModel obj)
+        {
+            return new RegistrationResultDataModel
+            {
+                Result = (RegistrationResultData)_chatDb.Registration(obj.Login, obj.Hash, obj.Email).FirstOrDefault()
+            };
+        }
+    }
+}
---
+++ ChatSolution/DataLevel/UserDataManager.cs
@@ -0,0 +1,19 @@
+﻿using DataLevel.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace DataLevel
+{
+    class UserDataManager : BaseDataManager
+    {
+        public UserDataModel GetCurrentUser(LoginDataModel obj)
+        {
+            Mapper.Initialize(cfg => cfg.CreateMap<GetUser_Result, UserDataModel>());
+            return Mapper.Map<GetUser_Result, UserDataModel>(_chatDb.GetUser(obj.Hash).FirstOrDefault());
+        }
+    }
+}
