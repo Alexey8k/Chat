f6ab7e23376d58622f486ed2915a54f9f15137cb
Home <home@gmail.com>
2017-08-12 00:11:32 +0300

    no message

--- ChatSolution/ChatClient/ViewModel/ChatViewModel.cs
+++ ChatSolution/ChatClient/ViewModel/ChatViewModel.cs
@@ -12,6 +12,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using LogicLevel;
+using LogicLevel.Mapping;
 using LogicLevel.Model;
 
 namespace ChatClient.ViewModel
