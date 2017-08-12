0306e1a1fbef498ff5ad88fe4320f90c6d614263
Home <home@gmail.com>
2017-08-12 22:59:43 +0300

    no message

---
+++ ChatSolution/ChatClient/Mapping/CurrentUserEventArgsExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using LogicLevel.EventArg;
+
+namespace ChatClient.Mapping
+{
+    public static class CurrentUserEventArgsExtension
+    {
+        public static T Mapping<T>(this CurrentUserEventArgs obj)
+        {
+            Mapper.Initialize(cfg => cfg.CreateMap<CurrentUserEventArgs, T>());
+            return Mapper.Map<CurrentUserEventArgs, T>(obj);
+        }
+    }
+}
